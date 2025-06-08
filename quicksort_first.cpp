21f4683d328264e62a1b0d66fa5bdd4f2773459c
Alina011304 <alina.babayan2@edu.ysu.am>
2025-06-08 11:53:49 +0400

    first commit

---
+++ quicksort_first.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+template <typename T>
+int partition(std::vector<T>& arr, int s, int e)
+{
+    T piv = arr[s];
+    int i = s + 1;
+
+    for (int j = s + 1; j <= e; ++j)
+    {
+        if (arr[j] < piv)
+        {
+            std::swap(arr[i], arr[j]);
+            ++i;
+        }
+    }
+
+    std::swap(arr[s], arr[i - 1]);
+    return i - 1;
+}
+
+template <typename T>
+void quickSort(std::vector<T>& arr, int s, int e)
+{
+    if (s < e)
+    {
+        int p = partition(arr, s, e);
+        quickSort(arr, s, p - 1);
+        quickSort(arr, p + 1, e);
+    }
+}
+
+int main()
+{
+    std::vector<int> data = {40, 80, 30, 90, 10, 50, 70};
+    quickSort(data, 0, data.size() - 1);
+
+    for (int num : data)
+        std::cout << num << " ";
+    return 0;
+}
